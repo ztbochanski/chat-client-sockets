5ee06db957f6b6e94cc017283056b7bf99ab406a
zach <ztbochanski@gmail.com>
2022-08-07 12:15:21 -0700

    add port and hostname tests

--- server/server_test.py
+++ server/server_test.py
@@ -4,18 +4,26 @@ from server import Server
 
 class TestServer(unittest.TestCase):
 
-    """
-    Initialization
-    """
-
     def test_instantiation(self):
-        """
+        '''
         A Server exists.
-        """
+        '''
         try:
             Server()
         except NameError:
-            self.fail("Could not instantiate Server.")
+            self.fail('Could not instantiate Server.')
+
+    def test_hostname(self):
+        '''
+        A server has a hostname.
+        '''
+        self.assertIsNotNone(Server().hostname)
+
+    def test_port(self):
+        '''
+        A server has a port.
+        '''
+        self.assertIsNotNone(Server().port)
 
 
 if __name__ == '__main__':
