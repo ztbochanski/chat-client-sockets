9d942f7c667b446d2e39349ba5a046478d74f9a8
zach <ztbochanski@gmail.com>
2022-08-07 12:04:58 -0700

    add algorithm for server

--- server/server.py
+++ server/server.py
@@ -1,3 +1,16 @@
 
 class Server:
-    pass
+    '''Server handles data streams through sockets.
+
+    Usage Algorithm:
+
+    STEP 1: Create socket to bind to host and port 'localhost:xxxx'
+    STEP 2: Listen for client connection
+    STEP 3: On connect, call receive data
+    STEP 4: Get reply, transmit data
+    STEP 5: On quit 'q' close server socket
+
+    '''
+
+    def __init__(self):
+        pass
