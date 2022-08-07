6357a91bd9adfef66e024efb4c3730852043416e
zach <ztbochanski@gmail.com>
2022-08-07 11:45:50 -0700

    add instantiation test

--- server/server_test.py
+++ server/server_test.py
@@ -3,4 +3,20 @@ from server import Server
 
 
 class TestServer(unittest.TestCase):
-    pass
+
+    """
+    Initialization
+    """
+
+    def test_instantiation(self):
+        """
+        A Server exists.
+        """
+        try:
+            Server()
+        except NameError:
+            self.fail("Could not instantiate Server.")
+
+
+if __name__ == '__main__':
+    unittest.main()
