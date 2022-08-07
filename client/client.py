3259cd9c86c28b61cb9dd69c355d039834cb67af
zach <ztbochanski@gmail.com>
2022-08-07 11:58:47 -0700

    add client program algorithm

---
+++ client/client.py
@@ -0,0 +1,11 @@
+'''Client transmits and receives data through sockets.
+
+Usage Algorithm:
+
+STEP 1: Create socket and connect to host: ex. 'localhost:xxxx'
+STEP 2: On connection, get input to send
+STEP 3: If message is quit exit client, close socket
+STEP 4: Else, transmit data
+STEP 5: Receive any data
+STEP 6: Display data
+'''
