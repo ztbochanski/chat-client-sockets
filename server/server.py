ba98d4ebc02ee4dd2712e18056704b077bc02a15
zach <ztbochanski@gmail.com>
2022-08-07 15:31:33 -0700

    remove comments and spaces that are not needed

--- server/server.py
+++ server/server.py
@@ -1,4 +1,3 @@
-
 import socket
 
 
@@ -12,13 +11,44 @@ class Server:
     STEP 3: On connect, call receive data
     STEP 4: Get reply, transmit data
     STEP 5: On quit 'q' close server socket
-
     '''
 
-    def __init__(self, hostname=socket.gethostname(), port=5000, socket=socket.socket()):
+    def __init__(self, hostname=socket.gethostname(), port=5000, server_socket=socket.socket()):
         self.hostname = hostname
         self.port = port
-        self.socket = socket
+        self.server_socket = server_socket
+
+    def on_connection(self):
+        connection, address = self.server_socket.accept()
+        print("Connection coming in from: " + str(address))
+        print('Waiting for message ...\n')
+
+        while True:
+            data = connection.recv(1024).decode()
+            if not data:
+                break
+            print("Message from Client: " + str(data))
+            data = input('Server: Enter a message > ')
+            connection.send(data.encode())
+
+        connection.close()
+
+    def listen(self, number_of_clients):
+        '''Bind the hostname and port and listen to a set number of clients.
+
+        :param number_of_clients: max size of allowed clients
+        '''
+        print('Server listening on:', self.hostname, 'port:', self.port)
+        self.bind_hostname_to_port()
+        self.listen_to_clients(number_of_clients)
 
     def bind_hostname_to_port(self):
-        self.socket.bind((self.hostname, self.port))
+        '''Bind together the hostname and port'''
+        self.server_socket.bind((self.hostname, self.port))
+
+    def listen_to_clients(self, number_of_clients):
+        '''Set listening size and listen to clients.
+
+        :param number_of_clients: max size allowed clients
+        '''
+        self.server_socket.listen(number_of_clients)
